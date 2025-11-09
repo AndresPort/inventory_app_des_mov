379f96b764dd3debfca2529bf7d16535048adcf8
Wil <wilmerulcuevelasco@gmail.com>
2025-11-08 22:39:55 -0500

    feat(edit-product): Criterio 2 - Mostrar ID desde argumentos de navegación

--- app/src/main/java/com/andresport/app_inventory/view/fragment/EditProductFragment.kt
+++ app/src/main/java/com/andresport/app_inventory/view/fragment/EditProductFragment.kt
@@ -3,24 +3,31 @@ package com.andresport.app_inventory.view.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.appbar.MaterialToolbar
+import androidx.navigation.fragment.navArgs
 import com.andresport.app_inventory.R
+import com.google.android.material.appbar.MaterialToolbar
 
-// El R.layout.fragment_edit_product debe coincidir con el nombre de tu archivo XML
 class EditProductFragment : Fragment(R.layout.fragment_edit_product) {
+
+    private val args: EditProductFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // --- Inicio---
+        // --- Criterio 1: Configuración de la Toolbar ---
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbarEditProduct)
         toolbar.setNavigationOnClickListener {
-            // Esta línea utiliza el Navigation Component para regresar a la pantalla anterior
-            // (HU 5.0: Detalle del producto)
             findNavController().navigateUp()
         }
 
+        // --- Criterio 2: Mostrar el ID del Producto ---
+        val tvProductValue = view.findViewById<TextView>(R.id.tvProductIdValue)
+
+        // Asigna el valor del ID. Por ahora, mostrará el valor por defecto "-1"
+        // si se llegara a esta pantalla sin pasar un ID.
+        tvProductValue.text = args.productId.toString()
     }
 }
