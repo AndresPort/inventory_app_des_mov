3ab93d041e6245db2ccdf9deacef497055ba9741
ninit <camilofernandezuno>
2025-11-21 07:51:02 -0500

    "criterio 2 y 3 H2 completados"

---
+++ app/src/main/java/com/andresport/app_inventory/view/UserSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.andresport.app_inventory.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andresport.app_inventory.R
+
+class UserSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_settings)
+    }
+}
